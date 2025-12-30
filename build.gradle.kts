48d50bdab12df60837729be10c42525672a2e068
pijushmanna <pijushmanna12344@gmail.com>
2025-12-31 00:25:13 +0530

    Upgraded Kotlin Verion - 2.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,13 @@
 plugins {
     alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.kotlin.kapt) apply false
 }
 
 allprojects {
     group = "com.github.codew1997"
-    version = "1.0.0"
+    version = "1.0.4"
 }
 
 subprojects {
